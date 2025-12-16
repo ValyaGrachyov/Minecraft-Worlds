ae56dc0608991a8c89936e28df0968af29ed5939
ValyaGrachyov <valentinegrachyov@gmail.com>
2025-12-16 03:01:49 +0300

    Add basic PRNG realization

---
+++ MinecraftWorldsAPI/PRNG/SeedMixer.cs
@@ -0,0 +1,30 @@
+﻿namespace MinecraftWorldsAPI.PRNG;
+
+public static class SeedMixer
+{
+    public static long Mix(long seed, long salt)
+    {
+        unchecked
+        {
+            long h = seed;
+            h ^= salt + (long) 0x9E3779B97F4A7C15;
+            h *= (long) 0xBF58476D1CE4E5B9;
+            h ^= h >> 27;
+            h *= (long) 0x94D049BB133111EB;
+            h ^= h >> 31;
+            return h;
+        }
+    }
+
+    public static long Mix(long seed, int x, int z)
+    {
+        unchecked
+        {
+            long h = seed;
+            h ^= x * 341873128712L;
+            h ^= z * 132897987541L;
+            return h;
+        }
+    }
+}
+
---
+++ MinecraftWorldsAPI/PRNG/XorShift64RandomService.cs
@@ -0,0 +1,53 @@
+﻿using MinecraftWorldsAPI.Interfaces;
+using MinecraftWorldsAPI.PRNG;
+
+namespace MinecraftWorldsAPI.Services.PRNG;
+
+public sealed class XorShift64Random : IRandom
+{
+    private ulong _state;
+
+    public long Seed { get; }
+
+    public XorShift64Random(long seed)
+    {
+        Seed = seed;
+        _state = (ulong)seed;
+
+        if (_state == 0)
+            _state = 0xdeadbeefcafebabeUL;
+    }
+
+    public uint NextUInt()
+    {
+        ulong x = _state;
+        x ^= x >> 12;
+        x ^= x << 25;
+        x ^= x >> 27;
+        _state = x;
+        return (uint)((x * 2685821657736338717UL) >> 32);
+    }
+
+    public int NextInt(int min, int max)
+    {
+        if (min > max)
+            throw new ArgumentOutOfRangeException();
+
+        if (min == max)
+            return min;
+
+        uint range = (uint)(max - min);
+        return (int)(NextUInt() % range) + min;
+    }
+
+    public double NextDouble()
+    {
+        return NextUInt() / (double)uint.MaxValue;
+    }
+
+    public IRandom Fork(long salt)
+    {
+        long forkSeed = SeedMixer.Mix(Seed, salt);
+        return new XorShift64Random(forkSeed);
+    }
+}
